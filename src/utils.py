c16e677de113921ef99c9882fe4bf8d8b64af331
iamcoolkorean <iamcoolkorean@gmail.com>
2026-05-31 18:35:27 +0900

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,14 @@
+from cryptography.fernet import Fernet
+import os
 
+def get_fernet():
+    key = os.getenv("ENCRYPTION_KEY")
+    return Fernet(key)
+
+def encrypt(plain_text: str) -> str:
+    f = get_fernet()
+    return f.encrypt(plain_text.encode()).decode()
+
+def decrypt(encrypted_text: str) -> str:
+    f = get_fernet()
+    return f.decrypt(encrypted_text.encode()).decode()
